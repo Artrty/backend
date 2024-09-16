5c0dbafc0205f157d8be518f7e09f8933fc85a3c
seohyeon <uimi3606@gmail.com>
2024-09-16 21:51:29 +0900

    ⚡ User 엔터티 수정

--- src/main/java/com/springboot/backend/Entity/User.java
+++ src/main/java/com/springboot/backend/Entity/User.java
@@ -8,25 +8,63 @@ import jakarta.persistence.Id;
 @Entity
 public class User {
 
-    @Id // user_UUID
+    @Id //기본키
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private int uuid;
 
-    private String password;
+    private String name; // 사용자 이름
+    private String phoneNumber; // 휴대폰 번호 (id로 사용)
+    private boolean phoneVerified; // 휴대폰 번호 확인 여부
+    private String password; // 비밀번호
+    private String college; // 대학교 (선택사항)
 
-    public Long getId() {
-        return id;
+
+    // Getter, Setter
+    public int getUuid() {
+        return uuid;
+    }
+
+    public void setUuid(int uuid) {
+        this.uuid = uuid;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 
     public String getPassword() {
         return password;
     }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
-
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public boolean isPhoneVerified() {
+        return phoneVerified;
+    }
+
+    public void setPhoneVerified(boolean phoneVerified) {
+        this.phoneVerified = phoneVerified;
+    }
+
+    public String getCollege() {
+        return college;
+    }
+
+    public void setCollege(String college) {
+        this.college = college;
     }
 }
